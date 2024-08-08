f7f0735d871e1f0483692aa8daab820a8457f6b2
AlexAzalor <xy55xy.azalor@gmail.com>
2024-08-08 10:43:55 +0300

    ukraine map draft

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,16 @@
 import { Header } from "@/components/header";
 import { HeroBlock } from "@/components/home/hero-block";
+import { UkraineMap } from "@/components/home/ukraine-map";
 
 export default function Home() {
   return (
-    <main className="max-w-maxDesktop mx-auto flex min-h-screen flex-col items-center border">
-      <div className="desktopEnd:px-0 w-full px-20">
+    <main className="mx-auto flex min-h-screen max-w-maxDesktop flex-col items-center border">
+      <div className="w-full px-20 desktopEnd:px-0">
         <Header />
         <HeroBlock />
       </div>
+
+      <UkraineMap />
     </main>
   );
 }
