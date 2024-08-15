f62a4452a530c433d1c2453ac33e691e5b45424a
AlexAzalor <xy55xy.azalor@gmail.com>
2024-08-15 10:46:05 +0300

    add faq block

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,11 @@
-import { AboutApp } from "@/components/home/about-app";
-import { Advantages } from "@/components/home/advantages";
-import { Categories } from "@/components/home/categories";
-import { ExpertsFeed } from "@/components/home/experts-feed/experts-feed";
-
 import { HeroBlock } from "@/components/home/hero-block";
-import { HowToStart } from "@/components/home/how-to-start";
 import { SearchBlock } from "@/components/home/search/search-block";
+import { Categories } from "@/components/home/categories";
+import { Advantages } from "@/components/home/advantages";
+import { AboutApp } from "@/components/home/about-app";
+import { ExpertsFeed } from "@/components/home/experts-feed/experts-feed";
+import { HowToStart } from "@/components/home/how-to-start";
+import { Faq } from "@/components/home/faq";
 
 export default function Home() {
   return (
@@ -20,6 +20,7 @@ export default function Home() {
       <AboutApp />
       <ExpertsFeed />
       <HowToStart />
+      <Faq />
     </main>
   );
 }
---
+++ src/components/home/faq.tsx
@@ -0,0 +1,64 @@
+import { useTranslations } from "next-intl";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export const Faq = () => {
+  const t = useTranslations("Home.faq");
+
+  const faqList = [
+    {
+      id: 1,
+      question: t("questions.first.question"),
+      answer: t("questions.first.answer"),
+    },
+    {
+      id: 2,
+      question: t("questions.second.question"),
+      answer: t("questions.second.answer"),
+    },
+    {
+      id: 3,
+      question: t("questions.third.question"),
+      answer: t("questions.third.answer"),
+    },
+    {
+      id: 4,
+      question: t("questions.fourth.question"),
+      answer: t("questions.fourth.answer"),
+    },
+    {
+      id: 5,
+      question: t("questions.fifth.question"),
+      answer: t("questions.fifth.answer"),
+    },
+  ];
+
+  return (
+    <div className="w-full px-20 py-28 desktopEnd:px-4 desktopEnd:py-20">
+      <h2 className="mb-14">{t("title")}</h2>
+
+      <Accordion type="single" collapsible className="">
+        {faqList.map((faq, i) => (
+          <AccordionItem
+            key={faq.id}
+            value={`item-${faq.id}`}
+            className="text-left"
+          >
+            <AccordionTrigger>
+              <div className="flex w-full items-center gap-8 text-left">
+                {i + 1 < 10 ? "0" : ""}
+                {i + 1}{" "}
+                <span className="text-2xl font-bold">{faq.question}</span>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>{faq.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+};
